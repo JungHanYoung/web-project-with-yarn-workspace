493fc830e67393a54822d9be1adc9a3e32d910ea
JeongHanYoung <8735132@gmail.com>
2019-06-14 22:17:18 +0900

    server - POST / 추가, concurrently module 추가

--- packages/server/index.js
+++ packages/server/index.js
@@ -3,9 +3,18 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     console.log('get이 불려졌습니다.')
     res.send('success')
+})
+
+app.post('/', (req, res) => {
+    const body = req.body
+    console.log(`username: ${body.username} / password: ${body.password}`)
+
+    res.send('post /')
 })
 
 app.listen(3000, () => {
