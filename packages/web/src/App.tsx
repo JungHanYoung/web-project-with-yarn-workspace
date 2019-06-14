029b978a8c8592cde373653aaf21e3af3972a354
JeongHanYoung <8735132@gmail.com>
2019-06-14 22:46:59 +0900

    web - React & Typescript, checking webpack build success

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+// UI 컴포넌트
+import Hello from './components/Hello'
+
+ReactDOM.render(<Hello />, document.getElementById('root'))
