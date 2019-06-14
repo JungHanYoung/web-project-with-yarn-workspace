5bb173092eb7f4d3dcbb9ad2bbc85dae1613306b
JeongHanYoung <8735132@gmail.com>
2019-06-14 21:51:49 +0900

    server - 간단한 '/' GET 요청 추가

--- packages/server/index.js
+++ packages/server/index.js
@@ -3,6 +3,11 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    console.log('get이 불려졌습니다.')
+    res.send('success')
+})
+
 app.listen(3000, () => {
     submodule()
 })
